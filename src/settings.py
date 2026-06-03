bb9b16792e2cbb2e482c5e807f06e208410a87de
Daniel Barutov <barutovdg@gmail.com>
2026-06-03 12:27:13 +0300

    copy-past

--- src/settings.py
+++ src/settings.py
@@ -1,10 +1,17 @@
+import os
+
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
-
-    database_url: str = "postgresql+asyncpg://postgres:postgres@localhost:5434/ads_db"
+    still_url: str | None = os.getenv("POSTGRES_CONNECTION_STRING")
+    if still_url:
+        database_url: str = still_url.replace("postgres:", "postgresql+asyncpg:", 1)
+    else:
+        database_url: str = (
+            "postgresql+asyncpg://postgres:postgres@localhost:5434/ads_db"
+        )
     jwt_secret: str = "change-me"
     jwt_algorithm: str = "HS256"
     kafka_bootstrap_servers: str = "localhost:9092"
